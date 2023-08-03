5d6e73cde8c96a2a6758a3a8d97e55e6dfc61923
KeremZeybek <recepkeremzeybek@gmail.com>
2023-08-03 17:10:44 +0300

    icon svg added

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -7,6 +7,7 @@ const ThemeContext = createContext();
 
 const ThemeContextProvider = ({ children }) => {
   const [myTheme, setMyTheme] = useState("light");
+  const values = {myTheme,setMyTheme};
 
   return (
     <ThemeContext.Provider value={{ myTheme, setMyTheme }}>
